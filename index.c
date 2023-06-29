ddd2f3bab1eb02bac759fa74d5dc94c2c7af1e16
souravdevloperr <xiaourav@gmail.com>
2023-06-30 01:48:25 +0600

    first commit

--- index.c
+++ index.c
@@ -2,6 +2,18 @@
 
 int main()
 {
-    printf("Hello World\n");
+    int arr[5];
+    for(int i =0; i<5; i++){
+        printf("Enter your %d element of array:",i+1);
+        scanf("%d", &arr[i]);
+    }
+    printf("\n \n");
+    int max = arr[0];
+    for(int i=0; i<5; i++){
+        if(max < arr[i]){
+            max = arr[i];
+        }
+    }
+    printf("The max element is: %d\n", max);
     return 0;
 }
